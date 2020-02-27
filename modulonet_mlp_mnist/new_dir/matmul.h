07e8e6c1edc4806525b1b0a7abe94eacdc2f2a4c
Afzal Ahmad <afzalahmad0203@gmail.com>
2020-02-27 13:59:49 +0500

    Untracked files issue fixed to resolved .gitignore

--- modulonet_mlp_mnist/new_dir/matmul.h
+++
@@ -1,8 +0,0 @@
-#ifndef MATMUL_H
-#define MATMUL_H
-#include <stdint.h>
-
-void matmul_int16_int16(uint32_t mat0_dim0, uint32_t mat0_dim1, uint32_t mat1_dim0, uint32_t mat1_dim1, int16_t mat0[][mat0_dim1], int16_t mat1[][mat1_dim1], int16_t out_mat[][mat1_dim1]);
-
-#endif
-
